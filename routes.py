4d3a843607741e8ccf00daee3bba5b304820889d
kc0836 <kc0836@gmail.com>
2018-07-24 22:42:01 +0800

    add signup form

--- routes.py
+++ routes.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template
+from models import db
+from forms import SignupForm
+import os
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route('/about')
+@app.route("/about")
 def about():
 	return render_template("about.html")
+
+@app.route("/signup")
+def signup():
+	form = SignupForm()
+	return render_template("signup.html",form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
